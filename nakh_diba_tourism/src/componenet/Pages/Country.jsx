42005748c2f0aa346a4fae919de1ad56fb49e4cb
abidkhan263187 <abidkhan263187@gmail.com>
2023-09-09 16:54:47 +0530

    add

--- nakh_diba_tourism/src/componenet/Pages/Country.jsx
+++ nakh_diba_tourism/src/componenet/Pages/Country.jsx
@@ -1,74 +1,100 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useSelector, } from 'react-redux'
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import axios from 'axios'
-import { Box, Button, Heading, Image } from '@chakra-ui/react'
+import { Box, Button, Heading, Image, Text } from '@chakra-ui/react'
 import { countryInfo } from '../Redux/action'
 import './style.css'
 import { Nav } from './Nav'
+import { StarIcon } from '@chakra-ui/icons'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+    slidesToSlide: 2 
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 1 
+    
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 1 
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 
+  }
+};
+export const ReviewCard = () => {
+  return (
+    <Box id="reviews_card" >
+      <Box id="review_card_left">
+        <Image src='https://avatars.githubusercontent.com/u/115173026?v=4' />
+      </Box>
+      <Box id="review_card_right">
+        <Heading size={'lg'}>Abid khan</Heading>
+        <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Iusto, fuga culpa. Provident corrupti voluptatem sint.
+        </Text>
+        <Text>
+          {Array(5)
+            .fill('')
+            .map((_, i) => (
+              <StarIcon
+                key={i}
+                color={i < 5 ? 'teal.500' : 'gray.300'}
+              />
+            ))}
+        </Text>
+      </Box>
+    </Box>
+  )
+}
 
 export const Country = () => {
-  const [imgArr,setImgArr]=useState([])
-  const [searchParams,setSearchParams]=useSearchParams()
+  const [imgArr, setImgArr] = useState([])
+  const [galArr, setGalArr] = useState([])
+  const [searchParams, setSearchParams] = useSearchParams()
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const [currentSlide, setCurrentSlide] = useState(0);
   const [countryArr, setCountryArr] = useState([])
 
-  const {region}=useParams()
-  const { continent } = useSelector((store) => {
-    return store;
-  })
-
+  const { region } = useParams()
 
 
   useEffect(() => {
     (async () => {
       try {
         await axios.get(`https://tired-cormorant.cyclic.app/tourism?Continent=${region}`).then(({ data }) => {
+          let images = data.data[0].images
+          let gallery = data.data[0].gallery
           let countriesarr = data.data[0].countries
           setCountryArr(countriesarr)
-          // console.log(countriesarr)
-          // console.log(countriesarr.CountryName)
-        })
-      } catch (error) {
-        console.log(error, "eroor while getting country")
-      }
-    })()
-  }, [])
-   useEffect(() => {
-    (async () => {
-      try {
-        await axios.get(`https://tired-cormorant.cyclic.app/tourism?Continent=${region}`).then(({ data }) => {
-          let images = data.data[0].images
+          setGalArr(gallery)
           setImgArr(images)
-          console.log(images)
-          // console.log(countriesarr.CountryName)
         })
       } catch (error) {
-        console.log(error, "eroor while getting country")
+        console.log(error, "eroor while getting arrays")
       }
     })()
   }, [])
- 
 
-  const handleCountry = (country) => {
 
-    // dispatch(countryInfo(country));
-    navigate(`/${region}/${country}`)
-  }
-
-  const prevSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === 0 ? imgArr.length - 1 : prevSlide - 1));
-  };
-
-  const nextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === imgArr.length - 1 ? 0 : prevSlide + 1));
-  };
+  const handleCountry = (country) => { navigate(`/${region}/${country}`) }
+  const prevSlide = () => { setCurrentSlide((prevSlide) => (prevSlide === 0 ? imgArr.length - 1 : prevSlide - 1)); };
+  const nextSlide = () => { setCurrentSlide((prevSlide) => (prevSlide === imgArr.length - 1 ? 0 : prevSlide + 1)); };
 
   useEffect(() => {
     const interval = setInterval(nextSlide, 2000);
@@ -83,20 +109,15 @@ export const Country = () => {
       <Nav />
       <div className="carousel-container">
         <div className="carousel">
-
           <div className='carousel-item'>
             <img src={imgArr[currentSlide]} alt={`Image`} />
           </div>
         </div>
-        <button colorScheme='blue' className="carousel-button prev" onClick={prevSlide}>
-          &#10094;
-        </button>
-        <button colorScheme='blue' className="carousel-button next" onClick={nextSlide}>
-          &#10095;
-        </button>
+        <button colorScheme='blue' className="carousel-button prev" onClick={prevSlide}> &#10094; </button>
+        <button colorScheme='blue' className="carousel-button next" onClick={nextSlide}> &#10095; </button>
       </div>
       <Box id="country_main" border={"1px solid red"}>
-        <Heading>{continent}</Heading>
+        <Heading>{region}</Heading>
         <Box id="country_Container">
           {countryArr && countryArr.length > 0 && countryArr.map((elem, ind) => {
             return (
@@ -111,6 +132,40 @@ export const Country = () => {
         </Box>
       </Box>
 
+      <Box>
+        <Heading>Gallery</Heading>
+        <Box id="gallery_container">
+          {galArr && galArr.length > 0 && galArr.map((elem, ind) => {
+            return (
+              <Box key={ind} className="card_container_galarry"  >
+                <Box className="card-image" as='img' src={elem}></Box>
+                <Box className="card-overlay">
+                  <h3 className="card-title" style={{ fontWeight: "700" }}>{elem.StateName}</h3>
+                  <p className="card-description">"Explore the states famous places</p>
+                  <Link to={'#'}> <Button bgColor={'#068181'} color={'white'} mt="10px" size="sm">See places</Button></Link>
+                </Box>
+              </Box>
+            )
+          })}
+        </Box>
+      </Box>
+
+
+      <Box>
+        <Heading>Reviews</Heading>
+        <Box id="review_container">
+          <Carousel responsive={responsive} 
+           swipeable={true}
+           showDots={true}
+           removeArrowOnDeviceType={["tablet", "mobile"]}
+            >
+            <ReviewCard />
+            <ReviewCard />
+            <ReviewCard />
+            <ReviewCard />
+          </Carousel>
+        </Box>
+      </Box>
     </div>
   )
 }
