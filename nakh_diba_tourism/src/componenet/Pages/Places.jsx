9fa9d8e8abbae3b7397026df37c7652007d00fb3
abidkhan263187 <abidkhan263187@gmail.com>
2023-09-14 20:39:29 +0530

    add

--- nakh_diba_tourism/src/componenet/Pages/Places.jsx
+++ nakh_diba_tourism/src/componenet/Pages/Places.jsx
@@ -1,15 +1,17 @@
-import { Box, Button, Heading, Image, Text } from '@chakra-ui/react';
+import { Badge, Box, Button, Flex, Heading, Image, Text, border } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { Nav } from './Nav';
 import { Slider } from './State';
+import { StarIcon } from '@chakra-ui/icons';
+import { Footer } from './Footer';
 export const Places = () => {
   const { state, country, region } = useParams();
   const [places, setPlaces] = useState([]);
-  const [hotel,setHotel]=useState([])
+  const [hotel, setHotel] = useState([])
+  const [gif,setGif]=useState("")
 
   useEffect(() => {
     (async () => {
@@ -17,8 +19,11 @@ export const Places = () => {
         const response = await axios.get(
           `https://tired-cormorant.cyclic.app/tourism?Continent=${region}&CountryName=${country}&StateName=${state}`
         );
+        const stateGif= response.data.data[0].state[0][0].stateGif
+        console.log(stateGif)
         const place = response.data.data[0].state[0][0].places;
         const hotels = response.data.data[0].state[0][0].hotels;
+        setGif(stateGif)
         setHotel(hotels)
         setPlaces(place);
         console.log(hotels);
@@ -31,57 +36,92 @@ export const Places = () => {
   return (
     <Box>
       <Nav />
-      <Slider />
+      {/* <Slider /> */}
+      <Box >
+        <Image width={"100%"} src={gif}/>
+      </Box>
       <Heading>{state}</Heading>
-      <Box  border={'1px solid blue'} h={'max-content'} gap={'10px'} display={'flex'} mb={'20px'} mt={"40px"}>
+      <Box h={'max-content'} gap={'10px'} display={'flex'} mb={'20px'} mt={"40px"}>
 
-    
-      <Box id="places_container" w={"70%"} >
-        {places.map((elem, ind) => (
-          <Box key={ind} id="place_main" display={'flex'} padding="20px">
-            <Box className="card_container"  >
-              <Box className="card-image" as='img' src="https://www.holidify.com/images/cmsuploads/compressed/Qutub_Minar_in_the_monsoons_20170908115259.jpg"></Box>
-              <Box className="card-overlay">
-                <h3 className="card-title" style={{ fontWeight: "700", color: "orange" }}>{elem.StateName}</h3>
-                <p className="card-description">Explore the  famous places</p>
-                <Link to={`#`}> <Button colorScheme={'orange'} color={'white'} mt="10px" size="sm">Know more</Button></Link>
+
+        <Box id="places_container" w={"70%"} >
+          {places.map((elem, ind) => (
+            <Box key={ind} id="place_main" >
+              <Box className="card_container"  >
+                <Box className="card-image" as='img' src={elem.images}></Box>
+                <Box className="card-overlay">
+                  <h3 className="card-title" style={{ fontWeight: "700", color: "orange" }}>{elem.StateName}</h3>
+                  <p className="card-description">Explore the  famous places</p>
+                  <Link to={`#`}> <Button colorScheme={'orange'} color={'white'} mt="10px" size="sm">Know more</Button></Link>
+                </Box>
+              </Box>
+
+              <Box id="place_right" >
+                <Heading size={'lg'} color={'gray.700'} > {elem.placeName}</Heading>
+                <Text color={"gray.600"}>{elem.desc}
+                </Text>
+                <Button size={'sm'} colorScheme='orange' variant={'outline'} mt={'20px'} as={Link} to={'#'}>Read more</Button>
               </Box>
             </Box>
-
-            <Box id="place_right" >
-              <Heading size={'lg'} color={'gray.700'} > {elem.placeName}</Heading>
-              <Text color={"gray.600"}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Voluptate tempora architecto sequi. Animi dolores molestiae odit
-                quis ullam ipsa dolor architecto totam quia incidunt, cupiditate,
-                delectus aperiam modi eum eveniet.
-              </Text>
-              <Button size={'sm'} colorScheme='orange' variant={'outline'} mt={'20px'} as={Link} to={'#'}>Read more</Button>
-            </Box>
-          </Box>
-        ))}
+          ))}
 
 
-      </Box>
-      <Box id="hotel_container">
-  {hotel && hotel.length > 0 && hotel.map((elem, ind) => {
-    return (
-      <Box display={'grid'} borderRadius={'10px'} gridTemplateColumns={'repeat'} border={'1px solid'} key={ind}>
-        <Box h={'50%'}>
-          <Image borderRadius={'10px 10px 0px 0px'} w={'100%'} src={elem.Image1} />
         </Box>
-        <Box p={' 10px'} textAlign={'start'}>
-          <Text>{elem.HotelName}</Text>
-          <Text>{elem.Mrp}</Text>
-          <Text>{elem.PriceRange}</Text>
-          <Text>{elem.Rating}</Text>
+        <Box id="hotel_container">
+          <Heading size={'md'}>Hotels</Heading>
+          {hotel && hotel.length > 0 && hotel.map((elem, ind) => {
+            return (
+              <Box p={'5px 5px 0px 5px'} margin={'auto'} maxW='sm' w={"90%"} borderWidth='1px' borderRadius='lg' >
+                <Image margin={"auto"} src={elem.Image} alt="img" />
+                <Box p='6' textAlign={'start'} >
+                  <Box display='flex' alignItems='baseline'>
+                    <Badge borderRadius='full' px='2' colorScheme='teal'>
+                      New
+                    </Badge>
+                    <Box
+                      color='gray.500'
+                      fontWeight='semibold'
+                      letterSpacing='wide'
+                      fontSize='xs'
+                      textTransform='uppercase'
+                      ml='2'
+                    >
+                      2 beds &bull; 3 baths
+                    </Box>
+                  </Box>
+
+                  <Box
+                    mt='1'
+                    fontWeight='semibold'
+                    as='h4'
+                    lineHeight='tight'
+                    noOfLines={1}
+                  >
+                    {elem.HotelName}
+                  </Box>
+
+                  <Flex alignItems={'center'} justifyContent={'space-between'} > <strong>${elem.PriceRange}</strong><Box as='span' color='gray.600' fontSize='sm'> <s id="s">${elem.Mrp}</s></Box>
+                    <p  style={{color:"green",fontWeight:"700"}}><strong>{elem.Discount} </strong>Off</p>
+                  </Flex>
+
+                  <Box display='flex' mt='2' alignItems='center'>
+                    {Array(5).fill('').map((_, i) => (
+                      <StarIcon key={i}
+                        color={i < elem.Rating ? 'orange.500' : 'gray.300'} />
+                    ))}
+                    <Box as='span' ml='2' color='gray.500' fontSize='sm'>
+                      {elem.Review} reviews
+                    </Box>
+                  </Box>
+                  <Button w={'100%'} colorScheme='orange' size={'sm'} mt={'10px'}>Book</Button>
+                </Box>
+              </Box>
+            )
+          })}
         </Box>
-      </Box>
-    )
-  })}
-</Box>
 
       </Box>
-      
+      <Footer />
     </Box>
   );
 };
