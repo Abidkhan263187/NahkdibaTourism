7eaf20e2eff59561b1966e679dd5ecbc20d7228a
abidkhan263187 <abidkhan263187@gmail.com>
2023-09-29 16:57:58 +0530

    #226

--- nakh_diba_tourism/src/componenet/Pages/BookForm.jsx
+++ nakh_diba_tourism/src/componenet/Pages/BookForm.jsx
@@ -140,7 +140,7 @@ export const BookingCard = () => {
                   isRequired
                 >
                   <option value="">Select</option>
-                  <option value="">0</option>
+                  <option value="0">0</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
