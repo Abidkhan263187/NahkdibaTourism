95c2eeedd7e93bbc160b198eb8755f44eed1aed5
abidkhan263187 <abidkhan263187@gmail.com>
2023-09-29 16:45:16 +0530

    #226

--- nakh_diba_tourism/src/componenet/Pages/State.jsx
+++ nakh_diba_tourism/src/componenet/Pages/State.jsx
@@ -7,9 +7,44 @@ import { Nav } from './Nav';
 import { Footer } from './Footer';
 import { BookCard } from './BookCard';
 
-const Events_News = [
-  // ...your event data...
-];
+export const Events_News = [
+  {
+    image: "https://www.planetware.com/wpimages/2019/09/india-amritsar-top-attractions-golden-temple.jpg",
+    info: "Golden Temple 1977",
+    headline: "Attack my goverment  duing the time  of the emergency",
+    details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam facere ratione."
+  },
+  {
+    image: "https://images.news18.com/ibnlive/uploads/2021/10/hawaii.jpg",
+    info: "Hawai Warthquake",
+    headline: "Attacked my Earthquake  duing the  summer event",
+    details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam facere ratione."
+  },
+  {
+    image: "https://www.tourmyindia.com/blog/wp-content/uploads/2021/03/Best-Places-to-Visit-in-Kerala.jpg",
+    info: "Africa Zambia ",
+    headline: "New ride starting this summer ",
+    details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam facere ratione."
+  },
+  {
+    image: "https://media.timeout.com/images/105761603/750/422/image.jpg",
+    info: "Italy",
+    headline: "Country is open to travel Around the world!",
+    details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam facere ratione."
+  },
+  {
+    image: "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2017/09/aquaventure-parmk-Copy.jpg",
+    info: "Dubai ",
+    headline: "Now you can enjoy summers and trips with your family ",
+    details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam facere ratione."
+  },
+  {
+    image: "https://www.planetware.com/wpimages/2022/02/japan-top-attractions-naritasan-shinsho-ji.jpg",
+    info: "Japan 2023",
+    headline: "Japan is givig free tourist visa for tourism huraay!",
+    details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsam facere ratione."
+  }
+]
 
 export const State = () => {
   const { country, region } = useParams();
